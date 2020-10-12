74077eb21b1de53eee2b1cc06fcee5c7fe01a241
Pavel Osadchuk <op@hrchatbot.ru>
2020-10-12 16:24:09 +0300

    Add Button and ButtonGroup and examples

---
+++ Command.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Xakpc.BlazorBits.Bootstrap
+{
+    /// <summary>
+    /// Very primitive simple generic command
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class Command<T> : ICommand
+    {
+        private readonly Action<T> _action;
+
+        public Command(Action<T> action)
+        {
+            _action = action;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(T parameter)
+        {                   
+            return true;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return CanExecute((T)parameter);
+        }
+
+        public void Execute(T parameter)
+        {
+            _action.Invoke(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            Execute((T)parameter);
+        }
+    }
+}
---
+++ Pages/Buttons.razor.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Xakpc.BlazorBits.Bootstrap.Pages
+{
+    /// <summary>
+    /// No view model here which is wrong
+    /// </summary>
+    public partial class Buttons
+    {
+        public Buttons()
+        {
+            // b
+            AddCommand = new Command<int>(DoAdd);
+
+            // bg
+            Items = new ObservableCollection<string>()
+            {
+                "Item 1",
+                "Item 2",
+                "Item 3"
+            };
+            Items.CollectionChanged += (sender, args) => StateHasChanged();
+
+            EditItemCommand = new Command<string>(s =>
+            {
+                if (Items.Contains(s))
+                    Items[Items.IndexOf(s)] = "Edited Item";
+            });
+            DeleteItemCommand = new Command<string>(s => Items.Remove(s));
+        }
+
+        private int Value { get; set; } = 10;
+
+        private System.Windows.Input.ICommand AddCommand { get; set; }
+
+        private void DoAdd(int p)
+        {
+            Value += p;
+            StateHasChanged(); // <--- nog gonna work without it
+        }
+
+        #region ButtonsGroup
+        private ObservableCollection<string> Items;
+
+        private System.Windows.Input.ICommand EditItemCommand { get; set; }
+
+        private System.Windows.Input.ICommand DeleteItemCommand { get; set; }
+        #endregion
+    }
+}
