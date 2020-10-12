80d38d68525133dd61cfdcde87354c875f79455d
Pavel Osadchuk <op@hrchatbot.ru>
2020-10-12 16:25:54 +0300

    Add vm basics

---
+++ ViewModels/ContextComponentBase.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace Xakpc.BlazorBits.Bootstrap.ViewModels
+{
+
+    /// <summary>
+    /// ComponentBase with DataContext
+    /// </summary>
+    public class ContextComponentBase : ComponentBase
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        protected ViewModelBase DataContext { get; set; }
+
+        /// <inheritdoc />
+        protected override async Task OnInitializedAsync()
+        {
+            if (DataContext == null) return;
+
+            if (!DataContext.Initialized) // do once
+            {
+                await DataContext.InitializeAsync();
+                DataContext.PropertyChanged += (s, e) => StateHasChanged();
+            }
+
+            await base.OnInitializedAsync();
+        }
+    }
+}
---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace Xakpc.BlazorBits.Bootstrap.ViewModels
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// Flag indicator if ViewModel was initialized. Set to true by InitializeAsync Method
+        /// Used to avoid double initialization for ServerPrerendered mode
+        /// </summary>
+        public bool Initialized { get; set; }
+
+        /// <summary>
+        /// Initialize view model
+        /// </summary>
+        /// <returns></returns>
+        /// <remarks>This method should not use any JSInterop if render mode is ServerPrerendered</remarks>
+        public virtual Task InitializeAsync()
+        {
+            Initialized = true;
+            return Task.CompletedTask;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="propertyName"></param>
+        // ReSharper disable once UnusedMember.Global
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
